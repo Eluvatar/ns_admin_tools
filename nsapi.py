5dd55bdd9109853eb3981cf5e975f9a5d5330119
Eluvatar <drstadler@gmail.com>
2013-03-16 17:12:48 -0400

    initial commit

---
+++ nsapi.py
@@ -0,0 +1,76 @@
+import xml.etree.ElementTree as ET
+from xml.parsers.expat import ExpatError as EE
+import urllib2,fcntl,time,logging
+
+from getpass import getuser
+try:
+  from os import uname
+except:
+  def uname():
+    return "Non-UNIX system?"
+from socket import gethostname
+
+uname_tuple=uname()
+uname_str=str((uname_tuple[0],uname_tuple[2],uname_tuple[4]))
+USER_AGENT=getuser()+'@'+gethostname()+" ("+uname_str+")"
+
+LOCK_FILE_PATH="/tmp/ns.lock"
+
+# Get the exclusive lock for talking to the NS API. This is a simple way
+# of ensuring you don't break the API Rate limit: All programs relying
+# on this lock will wait for it to be free before beginning.
+# Current semantics require programs to continue holding on to the lock for 30
+# seconds after they are done with it to allow programs to manage their own 
+# rate and not require a constant flat rate.
+LOCK_FILE=open(LOCK_FILE_PATH,'w+')
+fcntl.flock(LOCK_FILE,fcntl.LOCK_EX)
+
+logger = logging.getLogger(__name__)
+
+last_request=0.0
+def api_request(query,user_agent=USER_AGENT):
+  """ requests information from version 3 of the NS API. Raises an urllib2.HTTPError if the requested object does not exist or you have been banned from the API. """ 
+  global last_request
+  query['v']='3'
+  qs = map(lambda k: k+"="+(query[k] if isinstance(query[k],basestring) else "+".join(query[k])), query)
+  url="http://www.nationstates.net/cgi-bin/api.cgi?"+"&".join(qs)
+  req=urllib2.Request(url=url,headers={'User-Agent':user_agent})
+  logger.debug("Waiting to get %s", url)
+  now=time.time()
+  while( now < last_request + 0.625 ):
+    time.sleep( last_request + 0.625 - now )
+    now=time.time()
+  last_request=now
+  logger.debug("Getting %s", url)
+  try:
+    try:
+      return ET.parse(urllib2.urlopen(req))
+    except EE:
+      __handle_ee
+      raise
+  except urllib2.URLError as e:
+    if e.reason[0] == 110:
+      now=time.time()
+      last_request=now
+      logger.debug("Retrying %s", url)
+      try:
+        return ET.parse(urllib2.urlopen(req))
+      except EE:
+        __handle_ee(req)
+        raise
+    else:
+      raise
+
+def __handle_ee(req):
+  logger.error("api_request of %s failed to parse",url)
+  if logger.isEnabledFor(logging.DEBUG):
+    now=time.time()
+    while( now < last_request + 0.625 ):
+      time.sleep( last_request + 0.625 - now )
+      now=time.time()
+    last_request=now
+    f=urllib2.urlopen(req)
+    logger.debug("---begin---")
+    logger.debug(f.read())
+    logger.debug("---end---")
+
---
+++ wa_members_validator.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python
+from nsapi import api_request
+from ns import id_str
+from itertools import ifilterfalse
+import json,urllib2
+from getpass import getuser
+try:
+  from os import uname
+except:
+  def uname():
+    return "Non-UNIX system?"
+from socket import gethostname
+import logging
+
+logging.basicConfig(format="%(asctime)s %(message)s")
+logging.getLogger().setLevel(logging.DEBUG)
+
+uname_tuple=uname()
+uname_str=str((uname_tuple[0],uname_tuple[2],uname_tuple[4]))
+user_agent=getuser()+'@'+gethostname()+" WA members validator ("+uname_str+")"
+
+nat_is_wa = {}
+nat_exists = {}
+
+def check_wa_status(nat):
+  if nat not in nat_is_wa:
+    try:
+      natxml = api_request({'nation':nat,'q':'wa'},user_agent)
+      nat_is_wa[nat] = natxml.find('UNSTATUS').text != 'Non-member'
+      nat_exists[nat] = True
+    except urllib2.HTTPError:
+      nat_is_wa[nat] = False
+      nat_exists[nat] = False
+  return nat_is_wa[nat]
+
+def exists(nat):
+  if nat not in nat_exists:
+    check_wa_status(nat)
+  return nat_exists[nat]
+
+wa_xml = api_request({'wa':'1','q':'members'},user_agent)
+
+wa_members = wa_xml.find('MEMBERS').text.split(',')
+
+def page(nat):
+  i=wa_members.index(nat)
+  return i-i%15
+
+__page_url__fmts="http://www.nationstates.net/page=list_nations/un=UN?start=%d"
+def page_url(nat):
+  return __page_url__fmts%page(nat)
+
+wa_member_set=set(wa_members)
+
+# I'm relying on some other code I run nightly to generate a list of nations in
+# the WA as of the last major update which I run right after each major update
+# ends here, which makes a list of WA members in update order at the below URL.
+major_wa_members = map(id_str,json.load(urllib2.urlopen('http://www.thenorthpacific.org/api/wa.json')))
+major_wa_member_set=set(major_wa_members)
+
+new_wa_set = wa_member_set - major_wa_member_set
+ex_wa_set = major_wa_member_set - wa_member_set
+
+erroneously_listed = []
+erroneously_not_listed = []
+
+for nat in ifilterfalse(check_wa_status,new_wa_set):
+  erroneously_listed.append(nat)
+
+erroneously_not_listed = filter(check_wa_status,ex_wa_set)
+
+print "The following nations listed in http://www.nationstates.net/cgi-bin/api.cgi/wa=1/q=members are not WA nations:"
+print "[spoiler=%d nations erroneously listed][list=1]" % len(erroneously_listed)
+for nat in erroneously_listed:
+  if exists(nat):
+    cte=""
+  else:
+    cte="(CTE)"
+  print "[*][nation]%s[/nation] %s [[url=%s]page[/url]]"%(nat,cte,page_url(nat))
+
+print "[/list][/spoiler]"
+
+print "Worse, the following WA nations are not listed in [url]http://www.nationstates.net/cgi-bin/api.cgi/wa=1/q=members[/url]:"
+print "[spoiler=%d nations erroneously not listed][list=1]" % len(erroneously_not_listed)
+for nat in erroneously_not_listed:
+  print "[*][nation]%s[/nation]" % nat
+
+print "[/list][/spoiler]"
+
